8b6e786608fb43cd4f7acb73594272fb547173e9
binhex <megalith01@gmail.com>
2020-11-07 22:50:35 +0000

    initial push

---
+++ build/root/install.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# build scripts
+####
+
+# download build scripts from github
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
+
+# detect image arch
+####
+
+OS_ARCH=$(cat /etc/os-release | grep -P -o -m 1 "(?=^ID\=).*" | grep -P -o -m 1 "[a-z]+$")
+if [[ ! -z "${OS_ARCH}" ]]; then
+	if [[ "${OS_ARCH}" == "arch" ]]; then
+		OS_ARCH="x86-64"
+	else
+		OS_ARCH="aarch64"
+	fi
+	echo "[info] OS_ARCH defined as '${OS_ARCH}'"
+else
+	echo "[warn] Unable to identify OS_ARCH, defaulting to 'x86-64'"
+	OS_ARCH="x86-64"
+fi
+
+# pacman packages
+####
+
+# call pacman db and package updater script
+source upd.sh
+
+# define pacman packages
+# note gtk-doc needs to be included as its a requirement for github desktop,
+# but we put in a fix, thus install as a pre-req before the fix
+pacman_packages="git python2 python2-pip python2-packaging python3 python-pip python-packaging tk pycharm-community-edition nemo ttf-roboto gimp gtk-doc"
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aur packages
+####
+
+# fix for gconf not building (required for github-desktop-bin)
+mkdir -p '/usr/share/gtk-doc/data' && touch '/usr/share/gtk-doc/data/gtk-doc.make'
+
+# define aur packages
+aur_packages="google-chrome visual-studio-code-bin github-desktop-bin ttf-ms-fonts"
+
+# call aur install script (arch user repo)
+source aur.sh
+
+# config novnc
+###
+
+# overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
+cp /home/nobody/novnc-16x16.png /usr/share/webapps/novnc/app/images/icons/
+
+cat <<'EOF' > /tmp/startcmd_heredoc
+# launch apps
+google-chrome --disable-dev-shm-usage
+EOF
+
+# replace startcmd placeholder string with contents of file (here doc)
+sed -i '/# STARTCMD_PLACEHOLDER/{
+	s/# STARTCMD_PLACEHOLDER//g
+	r /tmp/startcmd_heredoc
+}' /home/nobody/start.sh
+rm /tmp/startcmd_heredoc
+
+# config openbox
+####
+
+cat <<'EOF' > /tmp/menu_heredoc
+    <item label="PyCharm">
+    <action name="Execute">
+      <command>pycharm</command>
+      <startupnotify>
+        <enabled>yes</enabled>
+      </startupnotify>
+    </action>
+    </item>
+    <item label="Google Chrome">
+    <action name="Execute">
+      <command>google-chrome --disable-dev-shm-usage</command>
+      <startupnotify>
+        <enabled>yes</enabled>
+      </startupnotify>
+    </action>
+    </item>
+    <item label="GitHub Desktop">
+    <action name="Execute">
+      <command>github-desktop</command>
+      <startupnotify>
+        <enabled>yes</enabled>
+      </startupnotify>
+    </action>
+    </item>
+    <item label="Microsoft VSCode">
+    <action name="Execute">
+      <command>code</command>
+      <startupnotify>
+        <enabled>yes</enabled>
+      </startupnotify>
+    </action>
+    </item>
+
+EOF
+
+# replace menu placeholder string with contents of file (here doc)
+sed -i '/<!-- APPLICATIONS_PLACEHOLDER -->/{
+    s/<!-- APPLICATIONS_PLACEHOLDER -->//g
+    r /tmp/menu_heredoc
+}' /home/nobody/.config/openbox/menu.xml
+rm /tmp/menu_heredoc
+
+# container perms
+####
+
+# define comma separated list of paths 
+install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/webapps/novnc,/usr/share/pycharm,/usr/share/applications,/etc/xdg"
+
+# split comma separated string into list for install paths
+IFS=',' read -ra install_paths_list <<< "${install_paths}"
+
+# process install paths in the list
+for i in "${install_paths_list[@]}"; do
+
+	# confirm path(s) exist, if not then exit
+	if [[ ! -d "${i}" ]]; then
+		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
+	fi
+
+done
+
+# convert comma separated string of install paths to space separated, required for chmod/chown processing
+install_paths=$(echo "${install_paths}" | tr ',' ' ')
+
+# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+chmod -R 775 ${install_paths}
+
+# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+cat <<EOF > /tmp/permissions_heredoc
+
+# get previous puid/pgid (if first run then will be empty string)
+previous_puid=\$(cat "/root/puid" 2>/dev/null || true)
+previous_pgid=\$(cat "/root/pgid" 2>/dev/null || true)
+
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different 
+# from the previous run then re-apply chown with current PUID and PGID values.
+if [[ ! -f "/root/puid" || ! -f "/root/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
+
+	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+	chown -R "\${PUID}":"\${PGID}" ${install_paths}
+
+fi
+
+# write out current PUID and PGID to files in /root (used to compare on next run)
+echo "\${PUID}" > /root/puid
+echo "\${PGID}" > /root/pgid
+
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+    s/# PERMISSIONS_PLACEHOLDER//g
+    r /tmp/permissions_heredoc
+}' /usr/local/bin/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
+
+# cleanup
+cleanup.sh
