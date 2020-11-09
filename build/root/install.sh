1909f0eee0dac7e79e3bca55474fe7910ddf549e
binhex <megalith01@gmail.com>
2020-11-09 13:34:47 +0000

    add imwheel to increase scroll speed

--- build/root/install.sh
+++ build/root/install.sh
@@ -40,7 +40,7 @@ source upd.sh
 # define pacman packages
 # note gtk-doc needs to be included as its a requirement for github desktop,
 # but we put in a fix, thus install as a pre-req before the fix
-pacman_packages="git python2 python2-pip python2-packaging python3 python-pip python-packaging tk pycharm-community-edition nemo ttf-roboto gimp gtk-doc docker nitrogen gsimplecal"
+pacman_packages="git python2 python2-pip python2-packaging python3 python-pip python-packaging tk pycharm-community-edition nemo ttf-roboto gimp gtk-doc docker nitrogen gsimplecal imwheel"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
